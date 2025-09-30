8f3d39ca28e5a63b6870d64ea6679c0090b22382
azan6155 <azan.naveedanwar@gmail.com>
2025-09-30 20:10:44 +0500

    Update q5.cpp

--- lab 04/q5.cpp
+++ lab 04/q5.cpp
@@ -1,1 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void sortSelection(vector<int> &nums)
+{
+    int len = nums.size();
+    for (int p = 0; p < len - 1; p++)
+    {
+        int minPos = p;
+        for (int q = p + 1; q < len; q++)
+        {
+            if (nums[q] < nums[minPos])
+            {
+                minPos = q;
+            }
+        }
+        int temp = nums[p];
+        nums[p] = nums[minPos];
+        nums[minPos] = temp;
+    }
+}
+
+int main()
+{
+    vector<int> years = {2022, 2023, 2024, 2022, 2023, 2024};
+
+    sortSelection(years);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < years.size(); i++)
+    {
+        cout << years[i] << " ";
+    }
+    cout << "\n";
+
+    return 0;
+}
 
